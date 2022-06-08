6c64d0f7f8f78c7c5e8eddf16c4b6d7568c2f783
Kim <dzhiqin@126.com>
2022-06-08 17:48:32 +0800

    feat: 添加multer

--- app.js
+++ app.js
@@ -1,33 +1,50 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-
+const multer  = require('multer')
+const PATH = './uploads/'
 
 // router import
 const user = require('./routes/user')
-
-
-
-
 const app = express();
-
 
 app.use(express.json())
 app.use(morgan('tiny'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, PATH);
+  },
+  filename: (req, file, cb) => {
+    const fileName = file.originalname.toLowerCase().split(' ').join('-');
+    cb(null, fileName)
+  }
+});
+const upload = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+      if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg" || file.mimetype == "image/gif") {
+          cb(null, true);
+      } else {
+          cb(null, false);
+          return cb(new Error('Allowed only .png, .jpg, .jpeg and .gif'));
+      }
+  }
+});
 // Routing
 app.use('/api', user)
-
 
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to  application." });
 });
 
-
+app.post('/add', upload.single('image'), (req, res, next) => {
+  console.log('upload res',res);
+  res.json({message: 'success'})
+})
 
 // set port, listen for requests
 const PORT = process.env.PORT || 3000;
