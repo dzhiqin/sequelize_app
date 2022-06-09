364b7ff61f6dfcf8bd3ca8997ac734c489f1d29a
Kim <dzhiqin@126.com>
2022-06-09 16:37:59 +0800

    feat: 添加日志管理

--- app.js
+++ app.js
@@ -3,13 +3,38 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const multer  = require('multer')
 const PATH = './uploads/'
-
+const fs = require('fs')
+const path = require('path')
 // router import
 const user = require('./routes/user')
 const app = express();
+const FileStreamRotator = require('file-stream-rotator')
 
+const logDirectory = path.join(__dirname, 'log')
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory)
+const rotateLogStream = FileStreamRotator.getStream({
+  date_format: 'YYYYMMDD',
+  filename: path.join(logDirectory,'access-%DATE%.log'),
+  frequency: 'daily',
+  verbose: false,
+  max_logs: 10,
+  size: '50k'
+})
+// const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'});
+// morgan.token('from',function(req,res){
+//   return req.query.from || '-'
+// })
+morgan.token('body',function(req,res){
+  return req.body ? JSON.stringify(req.body) : '-'
+})
+morgan.format('short', ':remote-addr :remote-user [:date[clf]] :method :body :url HTTP/:http-version :status :res[content-length] - :response-time ms');
 app.use(express.json())
-app.use(morgan('tiny'));
+app.use(morgan('short', {
+  stream: rotateLogStream,
+  // stream: accessLogStream,
+  // skip: function(req,res) { return res.statusCode < 400 }
+}));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
